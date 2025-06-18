3bab2b02bbf3c520e06021c53b981f96bba700a9
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-06-19 01:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.21%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,22 @@
 class Solution{
-
-private:
-int find(int ind,vector<int>& nums,vector<int>& dp){
-     if (ind < 0) return 0;
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        
-        int pick=find(ind-2,nums,dp)+abs(nums[ind]);
-         int pick2=find(ind-1,nums,dp);
-
-return dp[ind]=max(pick,pick2);
-    }
-    
 public:
     int rob(vector<int>& nums) {
-     int n =nums.size();
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-     vector<int>dp(n,-1);
-     return find(n-1,nums,dp);
-    }
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+    
+     for(int i=2;i<n;i++){
+        int pick=dp[i-2]+nums[i];
+         int pick2=dp[i-1];
+         dp[i]=max(pick,pick2);
+
+     }
+     return dp[n-1];
+
+ }
 };
 
