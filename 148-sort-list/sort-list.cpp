5209047115453f6927f99736a6dc2a5612fd386b
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-06-25 02:46:34 +0530

    Time: 43 ms (50.46%) | Memory: 75.7 MB (37.84%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        ListNode* dummy=new ListNode(INT_MIN);
+        ListNode* curr=dummy;
+        while(l1 && l2){
+            if(l1->val>=l2->val){
+                curr->next=l2;
+                l2=l2->next;
+            }
+            else{
+                curr->next=l1;
+                l1=l1->next;
+            }
+            curr=curr->next;
+        }
+        if(l1) curr->next=l1;
+        else curr->next=l2;
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* leftHead=head;
+        ListNode* rightHead=slow->next;
+        slow->next=nullptr;
+        return merge(sortList(leftHead) ,sortList(rightHead));
+
+    }
+};
