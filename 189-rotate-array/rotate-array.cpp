92de43d4a0654d84bc1ea08679b9b2282b775c3a
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-06-25 11:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (97.00%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,26 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        vector<int> temp(k);
 
-    
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
-        }
-
-        
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i - k];
-        }
-
-     
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
-
-       
-        for (int i = 0; i < n; i++) {
-            cout << nums[i] << " ";
-        }
+        reverse(nums.begin(), nums.end()); 
+        reverse(nums.begin(), nums.begin() + k); 
+        reverse(nums.begin() + k, nums.end()); 
     }
 };
