6dd2fc40190f0427b312bc13bc2b5ce9d3bb0d0b
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-06-26 18:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (53.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,16 +2,44 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        map<int,int>mpp;
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
+      int el1=INT_MIN;
+      int el2=INT_MIN;
+      int count1=0;
+      int count2=0;
+
+      for(int i=0;i<n;i++){
+        if (count1==0 && nums[i]!=el2){
+        count1=1;
+        el1=nums[i];
         }
-vector<int>result;
-        for(auto it:mpp){
-            if(it.second>n/3){
-                result.push_back(it.first);
-            }
+        else if(count2==0 && nums[i]!=el1){
+             count2=1;
+        el2=nums[i];
         }
-        return result;
+        else if(nums[i]==el1){
+            count1++;
+        }
+        else if(nums[i]==el2){
+            count2++;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+      }
+   int count3=0;
+   int count4=0;
+   vector<int>result;
+  for(int i=0;i<n;i++){
+    if(el1==nums[i])count3++;
+     if(el2==nums[i])count4++;
+  }
+  if(count3>n/3){
+     result.push_back(el1);
+  }
+  if(count4>n/3){
+    result.push_back(el2);
+  }
+  return result;
     }
 };
