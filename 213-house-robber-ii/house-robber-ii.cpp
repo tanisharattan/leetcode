c1ab2b8916b6876c2dafd6922f5e1168ed85656d
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-01 23:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (39.84%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,36 +1,31 @@
 class Solution {
     private:
-    int recurs(vector<int>& dp,vector<int>& nums,int ind){
-        if(ind==0){
-            return nums[ind];
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-
-int pick = nums[ind]+recurs(dp,nums,ind-2);
-int notPick = 0+recurs(dp,nums,ind-1);
-return dp[ind]= max(pick , notPick);
+ int recurs(vector<int>&temp,int i ,vector<int>& dp){
+    if(i==0){
+        return temp[i];
     }
+    if(i<0){
+        return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int pick =temp[i]+recurs(temp,i-2,dp);
+    int notPick=0+recurs(temp,i-1,dp);
+    return dp[i]=max(pick , notPick);
+ }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-if (n == 1) return nums[0];
-        vector<int>temp1(nums.begin()+1,nums.end());
-        vector<int>temp2(nums.begin(),nums.end()-1);
+    int n = nums.size();
+    if (n == 1) return nums[0];
+    vector<int>dp(n,-1);
+vector<int>temp1(nums.begin(),nums.end()-1);
+vector<int>temp2(nums.begin()+1,nums.end());
+vector<int>dp1(n-1,-1);
+vector<int>dp2(n-1,-1);
+     int option1=recurs(temp1,n-2,dp1);
+     int option2=recurs(temp2,n-2,dp2);
 
-        vector<int>dp1(n-1,-1);
-         vector<int>dp2(n-1,-1);
-
-         int option1=recurs(dp1,temp1,n-2);
-         int option2=recurs(dp2,temp2,n-2);
-
-         return max(option1,option2);
-
-     
+     return max(option1,option2);
     }
 };
