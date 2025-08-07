bbfca8ef35460add0fe22fe456a87a3a04f031c4
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-08 00:08:45 +0530

    Time: 7 ms (73.46%) | Memory: 44.6 MB (95.74%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+     int n = g.size();
+     int m = s.size();
+sort(g.begin(),g.end());
+sort(s.begin(),s.end());
+int i =0, j =0;
+
+while(i<n && j <m){
+    if(g[i]<=s[j]){
+        i=i+1;
+    }
+
+    j=j+1;
+
+    }
+
+    return i;
+    
+    }
+
+};
