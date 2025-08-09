9a38c566e909d0c237c19221ae673292c495e407
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-09 23:11:11 +0530

    Time: 31 ms (65.21%) | Memory: 31.5 MB (58.45%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,29 @@
 class Solution {
 private:
-    int lps(int i, int j, string &s, vector<vector<int>> &dp) {
-        if (i > j) return 0;
-        if (i == j) return 1;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s[i] == s[j]) {
-            return dp[i][j] = 2 + lps(i + 1, j - 1, s, dp);
-        }
-        return dp[i][j] = max(lps(i + 1, j, s, dp), lps(i, j - 1, s, dp));
+int find( string&s, vector<vector<int>>&dp , int i , int j){
+    if(i==j){
+        return 1;
     }
+
+    if(i>j){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+
+    }
+
+    if(s[i]==s[j]){
+        return dp[i][j]=2+find(s, dp , i+1,j-1);
+    }
+    return dp[i][j]=max(find(s , dp , i+1, j), find(s,dp,i,j-1));
+}
 
 public:
     int minInsertions(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int longestPalSubseq = lps(0, n - 1, s, dp);
-        return n - longestPalSubseq;
+       int n = s.length();
+       vector<vector<int>>dp(n,vector<int>(n,-1));
+
+      return n - find( s , dp , 0 , n-1);
     }
 };
