4fba7bcf3535b4e513b3f0e7b104bbe5dc04f124
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-12 14:56:37 +0530

    Time: 4 ms (43.20%) | Memory: 14.2 MB (54.25%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
-    void helper (vector<int>& candidates,int ind , int target, vector<vector<int>>&result, vector<int>&temp ){
+    private:
+    void help(vector<int>& candidates, int target , vector<int>&temp , vector<vector<int>>&result, int ind){
         if(target==0){
             result.push_back(temp);
         }
-
-        for(int i =ind ; i<candidates.size();i++){
-            if(candidates[i]>target)break;
-           temp.push_back(candidates[i]);
-           helper(candidates,i, target-candidates[i], result , temp);
-           temp.pop_back();
+        for(int i = ind ; i <candidates.size();i++){
+            if(candidates[i]>target)return ;
+            temp.push_back(candidates[i]);
+            help(candidates , target-candidates[i], temp , result ,i );
+            temp.pop_back();
         }
+
     }
+  
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>result ;
-        vector<int>temp;
-        sort(candidates.begin(),candidates.end());
-        helper(candidates,0, target, result , temp);
-        return result;
+       vector<vector<int>>result ;
+       vector<int>temp ;
+       sort(candidates.begin(),candidates.end());
+       help(candidates , target , temp , result , 0 );
+      return result ;
     }
 };
