869a766d5bdce7dbeddbbded6b2765ebd1bee847
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-18 03:46:06 +0530

    Time: 471 ms (20.87%) | Memory: 8.8 MB (26.74%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
- void solveSudoku(vector<vector<char>>& board){
-    solve(board);
- }
-bool isValid( int row , int col ,vector<vector<char>>& board, char c ){
+bool isValid(int row , int col , char c,vector<vector<char>>& board){
     for(int i =0;i<9;i++){
         if(board[i][col]==c)return false;
         if(board[row][i]==c)return false;
-       if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
         return false;
     }
     return true;
 }
+
     bool solve(vector<vector<char>>& board) {
         
-        for(int i =0;i<board.size();i++){
-            for(int j =0;j<board[0].size();j++){
-                       if(board[i][j]=='.'){
-                        for(char c='1';c<='9';c++ ){
-                            if(isValid(i , j , board , c )){
-                                board[i][j]=c;
-
-                                if(solve(board)==true)return true;
-                                board[i][j] = '.';
-                            }
-                        }
-                        return false;
-                       }
+        int n = board.size();
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9' ;c++ ){
+                         if(isValid(i , j , c ,board )){
+                            board[i][j]=c;
+                            if (solve(board)) return true;
+                            board[i][j]='.';
+                         }
+                        
+                    }
+                     return false;
+                }
             }
         }
-        return true;
+     return true;
     }
+     void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+     }
 };
