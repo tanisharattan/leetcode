8b4db5728123dc5a1c9024b4a55655da5af9e9b1
Tanisha Rattan <154467194+tanisharattan@users.noreply.github.com>
2025-08-22 02:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        unordered_map<char , int> count ;
+        for(auto x : s){
+            count[x]++;
+        }
+         for(auto x : t){
+            count[x]--;
+        }
+        for(auto x :count){
+            if(x.second!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
